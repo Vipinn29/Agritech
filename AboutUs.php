c3bf14a16e9614df9039bcbbd24f68cf505181da
Vipinn29 <vipingupta2k4@gmail.com>
2024-05-12 14:08:38 +0530

    new page

--- AboutUs.php
+++ AboutUs.php
@@ -1,3 +1,60 @@
-<p>This Page Gives Visitors An Insight Into Our Business, Its History And Its Values. It Can Include Information About Our Team, Your Mission And Vision Statements, And Your Unique Selling Proposition.
+<?php
+
+session_start();
+include_once("admin/class/adminback.php");
+$obj = new adminback();
+
+$cata_info = $obj->p_display_catagory();
+$cataDatas = array();
+while ($data = mysqli_fetch_assoc($cata_info)) {
+    $cataDatas[] = $data;
+}
+
+
+
+
+$pdt_info = $obj->view_all_product();
+
+$pdt_datas = array();
+        
+while($pdt_ftecth = mysqli_fetch_assoc($pdt_info)){
+            $pdt_datas[] = $pdt_ftecth;
+}
+
+
+
+
+?>
+
+
+<?php
+include_once("includes/head.php");
+?>
+
+<body class="biolife-body">
+    <!-- Preloader -->
+
+    <?php
+    include_once("includes/preloader.php");
+    ?>
+
+    <!-- HEADER -->
+    <header id="header" class="header-area style-01 layout-03">
+
+        <?php
+        include_once("includes/header_top.php");
+        ?>
+
+        <?php
+        include_once("includes/header_middle.php");
+        ?>
+
+        <?php
+        include_once("includes/header_bottom.php");
+        ?>
+
+    </header>
+
+    <p>This Page Gives Visitors An Insight Into Our Business, Its History And Its Values. It Can Include Information About Our Team, Your Mission And Vision Statements, And Your Unique Selling Proposition.
 
 At Koytur Fish Farming Private Limited, We Take Pride In What We Do. We’re Not Just A Company, We’re A Community Of People Who Are Passionate About Providing High-Quality Fish To The People India, And We’re Just Getting Started.</p>
