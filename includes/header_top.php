adf752f7a3a2a09dbaffde78092c29af1c050605
Vipinn29 <vipingupta2k4@gmail.com>
2024-05-16 00:46:17 +0530

    nn

--- includes/header_top.php
+++ includes/header_top.php
@@ -24,7 +24,7 @@ $obj= new adminback();
 
                    
                         </i>
-                        <li><a href="#">Free Shipping</a></li>
+                        <li><a href="#">Shipping all over India</a></li>
                     </ul>
                 </div>
                 <div class="top-bar right">
