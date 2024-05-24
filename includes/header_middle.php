d9096a0004e813aff197b1e60984e4b0a2125e91
Vipinn29 <vipingupta2k4@gmail.com>
2024-05-24 20:08:05 +0530

    tracking systm

--- includes/header_middle.php
+++ includes/header_middle.php
@@ -57,6 +57,8 @@ if (isset($_POST['remove_product'])) {
                             if(isset($_SESSION['user_id'])){
                                  ?>
                                   <li class="menu-item"><a href="exist_order.php">Dashboard </a></li>
+                                  <li class="menu-item"><a href="./Orders/index.php">Orders </a></li>
+
                                  <?php
                             }
                         ?>
