4a1de4eac7957158dd78ed72025e8a2571a7f4b4
Ekant Dewangan <123737970+ekantdew05@users.noreply.github.com>
2024-08-24 14:27:13 +0530

    categories and text color dne

--- catagory.php
+++ catagory.php
@@ -124,8 +124,9 @@ include_once("includes/head.php");
 
                                             </div>
                                             <div class="shipping-info">
-                                                <p class="shipping-day">Same Day Shipping</p>
-                                                <p class="for-today">Free Pickup Today</p>
+                                                <p class="shipping-day">This price is valid for minimum 10 kg order</p>
+                                                <p class="for-today">If you order today, your delivery will arrive by tomorrow</p>
+                                                >
                                             </div>
                                             <div class="slide-down-box">
                                                 <p class="message">All products are carefully selected to ensure fish safety.</p>
